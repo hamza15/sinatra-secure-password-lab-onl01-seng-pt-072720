49d0ad1eb2ba208fe67625c7d4772983168ab271
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-11-02 08:24:46 +0000

    Automatically backed up by Learn

--- db/migrate/20201102082314_create_users_table.rb
+++ db/migrate/20201102082314_create_users_table.rb
@@ -2,6 +2,6 @@ class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string ""
+      t.string :password_digest
   end
 end

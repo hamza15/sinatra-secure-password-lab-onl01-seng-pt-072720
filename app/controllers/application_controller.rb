ce81bbe2bc80ce50ebbc1463d87ec08a52616e36
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-11-02 08:28:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,11 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     @user = User.new(:username => params[:username], :password => params[:password])
     
-    if user
-    #your code here
+    if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
 
   end
 
